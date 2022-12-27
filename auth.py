46fa683c4e998a740c8f0859fe0871c6b85f1c1f
RasoulZamani <r.zamani72@gmail.com>
2022-12-27 16:43:04 +0330

    creating new user

---
+++ auth.py
@@ -0,0 +1,31 @@
+""" auth.py for creating new user and authentication"""
+
+# import libs 
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+import model
+
+class CreateUser(BaseModel):
+    """ create base model for user"""
+    username: str
+    email: Optional[str]
+    first_name: str
+    last_name: str
+    password: str
+
+app = FastAPI()
+
+@app.post("/create/user")
+async def create_new_user(create_user: CreateUser):
+    """creating new user"""
+    create_user_model = model.User()
+    create_user_model.email = create_user.email
+    create_user_model.username = create_user.username
+    create_user_model.first_name = create_user.first_name
+    create_user_model.last_name = create_user.last_name
+    create_user_model.hashed_pass = create_user.password 
+    create_user_model.is_active = True
+    
+    return create_user_model
+    
