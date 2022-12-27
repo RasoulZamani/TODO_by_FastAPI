0d63d7e1c3e701e7ea8cbb30e21c161ed5fb20df
RasoulZamani <r.zamani72@gmail.com>
2022-12-27 11:00:10 +0330

    get item by id (path param)

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ http://127.0.0.1:8000/docs for interactive test
 # import libs
 import model
 from database import engine, SessionLocal
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 
@@ -28,8 +28,27 @@ def get_db():
     finally:
         db.close()
         
-
+# read all data
 @app.get('/')
 async def read_all_list(db: Session=Depends(get_db)):
     """first get database and then reading all jobs in todo table """ 
     return db.query(model.Todo).all()
+
+
+# read data with specified id
+@app.get("/todo/{todo_id}")
+async def read_todo_by_id(todo_id: int, db: Session = Depends(get_db)):
+    """first get database and then read data with specified id"""
+    result = db.query(model.Todo)\
+            .filter(model.Todo.id == todo_id)\
+            .first()
+    if result is not None:
+        return result
+    raise not_found_exception()
+
+
+
+def not_found_exception():
+    """if item in todo table not found, we'll raise exception"""
+    return HTTPException(status_code=404, 
+                         detail="Oops! Todo item not found!")
