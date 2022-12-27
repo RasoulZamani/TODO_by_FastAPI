cadd6f85e73b0c26ac4b901e9068cf1180023eb8
RasoulZamani <r.zamani72@gmail.com>
2022-12-27 18:15:53 +0330

    authenticate user

--- auth.py
+++ auth.py
@@ -5,20 +5,35 @@ ant authentication stuff
 """
 
 # import libs _________________________________________________________
+import model
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from typing import Optional
 from passlib.context import CryptContext
 from database import engine, SessionLocal
-import model
 
-# hashing contex
-bcrypt_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
- 
+
 app = FastAPI()
 # create table if auth run before main
 model.Base.metadata.create_all(bind=engine)
+
+# hashing contex
+bcrypt_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# authenticate user
+def authenticate_user(username: str, password: str, db):
+    """ authenticate user"""
+    user = db.query(model.User)\
+        .filter(model.User.username == username)\
+        .first()
+    if not user:
+        return False
+    if not bcrypt_ctx.verify(password, user.hashed_pass):
+        return False
+
+    return user 
 
 
 def get_db():
@@ -55,9 +70,18 @@ async def create_new_user(create_user: CreateUser, db: Session=Depends(get_db)):
     db.commit()
     
     return successful_response(200)
-    
-    
-    
+
+@app.post("/token")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),
+                                 db: Session = Depends(get_db)):
+    """login for access token"""
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=404, 
+                         detail="Oops!User not found or Password is wrong!")
+    return "User Valdated Successfuly!"   
+           
+# common function for reusing__________________________________________
 
 # successful response
 def successful_response(status_code: int):
@@ -66,4 +90,12 @@ def successful_response(status_code: int):
         "status": status_code,
         "transaction": "Successful"
     } 
+
+      
+# exception not found
+def not_found_exception():
+    """if item in todo table not found, we'll raise exception"""
+    return HTTPException(status_code=404, 
+                         detail="Oops!not found!")
+
     
