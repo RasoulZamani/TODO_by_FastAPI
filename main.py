55ea91f44e5189ac0bb9555b00fa07d2618834b3
RasoulZamani <r.zamani72@gmail.com>
2022-12-27 11:55:34 +0330

    adding update method

--- main.py
+++ main.py
@@ -39,14 +39,14 @@ class Todo(BaseModel):
     priority: int = Field(gt=0, lt=6, description="the priority must be between 1-5")
     complete: bool
         
-# read all data
+# read all data _______________________________________________________
 @app.get('/')
 async def read_all_list(db: Session=Depends(get_db)):
     """first get database and then reading all jobs in todo table """ 
     return db.query(model.Todo).all()
 
 
-# read data with specified id
+# read data with specified id _________________________________________
 @app.get("/todo/{todo_id}")
 async def read_todo_by_id(todo_id: int, db: Session = Depends(get_db)):
     """first get database and then read data with specified id"""
@@ -58,6 +58,7 @@ async def read_todo_by_id(todo_id: int, db: Session = Depends(get_db)):
     raise not_found_exception()
 
 
+# post new todo item __________________________________________________ 
 @app.post("/")
 async def create_todo_item(todo: Todo, db: Session = Depends(get_db)):
     """after connecting to db, create todo item and adding to db"""
@@ -76,7 +77,34 @@ async def create_todo_item(todo: Todo, db: Session = Depends(get_db)):
         "transaction": "Successful"
     }
     
-
+ 
+# put (update) item with specified id
+@app.put("/{todo_id}")
+async def update_item(todo_id: int, todo: Todo, db: Session = Depends(get_db)):
+    """after connecting to db, updating todo item"""
+    todo_model = db.query(model.Todo)\
+            .filter(model.Todo.id == todo_id)\
+            .first()
+    if todo_model is None:
+        raise not_found_exception()
+    
+    
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+    
+    db.add(todo_model)
+    db.commit()
+    
+    return {
+        "status": 200,
+        "update": "Successful"
+    }
+     
+    
+    
+# exception not found
 def not_found_exception():
     """if item in todo table not found, we'll raise exception"""
     return HTTPException(status_code=404, 
