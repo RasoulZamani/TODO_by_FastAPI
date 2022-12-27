c2d6b9553111bfeb3b00a552d24e46be3d58946d
RasoulZamani <r.zamani72@gmail.com>
2022-12-27 08:15:56 +0330

    creating database by sqlalchemy and sqlite

---
+++ database.py
@@ -0,0 +1,17 @@
+""" database.py is for creating database by SqlAlchemy"""
+
+# import libs:
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# create todo.db database
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
+
+engine = create_engine( SQLALCHEMY_DATABASE_URL ,
+                       connect_args={"check_same_thread":False})
+
+SessionLocal = sessionmaker(autocommit=False,
+                            autoflush=False, bind=engine)
+
+Base = declarative_base()
