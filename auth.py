483ae5545101c6f6963738e0a88ac7295cf53060
RasoulZamani <r.zamani72@gmail.com>
2022-12-27 16:58:54 +0330

    hashin password by bcrypt

--- auth.py
+++ auth.py
@@ -1,11 +1,17 @@
-""" auth.py for creating new user and authentication"""
+""" 
+auth.py for creating new user
+and hashing password by bcrypt 
+ant authentication stuff
+"""
 
-# import libs 
+# import libs _________________________________________________________
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
+from passlib.context import CryptContext
 import model
 
+# base model for user _________________________________________________
 class CreateUser(BaseModel):
     """ create base model for user"""
     username: str
@@ -14,6 +20,9 @@ class CreateUser(BaseModel):
     last_name: str
     password: str
 
+# hashing contex
+bcrypt_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
+ 
 app = FastAPI()
 
 @app.post("/create/user")
@@ -24,7 +33,7 @@ async def create_new_user(create_user: CreateUser):
     create_user_model.username = create_user.username
     create_user_model.first_name = create_user.first_name
     create_user_model.last_name = create_user.last_name
-    create_user_model.hashed_pass = create_user.password 
+    create_user_model.hashed_pass = bcrypt_ctx.hash(create_user.password) 
     create_user_model.is_active = True
     
     return create_user_model
