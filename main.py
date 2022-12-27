a47c3a4ca7e976d93a06b25cd0c92a1499ea9b46
RasoulZamani <r.zamani72@gmail.com>
2022-12-27 10:44:21 +0330

    adding get methode for read all rows of database

--- main.py
+++ main.py
@@ -9,11 +9,27 @@ http://127.0.0.1:8000/docs for interactive test
 
 # import libs
 import model
-from database import engine
-from fastapi import FastAPI
+from database import engine, SessionLocal
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
 
 # instantiate app
 app = FastAPI()
 
 # create table
 model.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    """ geeting database either successfully or not and close it """
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+        
+
+@app.get('/')
+async def read_all_list(db: Session=Depends(get_db)):
+    """first get database and then reading all jobs in todo table """ 
+    return db.query(model.Todo).all()
