7a36662325f63dda79af810c5ad559958ed2d93c
RasoulZamani <r.zamani72@gmail.com>
2022-12-27 08:36:36 +0330

    database for app by sqlalchemy

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# create todo.db database
+# todo.db database
 SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
 
 engine = create_engine( SQLALCHEMY_DATABASE_URL ,
