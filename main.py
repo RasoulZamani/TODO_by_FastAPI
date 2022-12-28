58885cbb98ff8b804831ab04006a4943524b74b2
RasoulZamani <r.zamani72@gmail.com>
2022-12-28 20:53:44 +0330

    external routing for company

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ http://127.0.0.1:8000/docs for interactive test
 # import libs
 import model
 from database import engine
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
 from router import auth, todo_apis
-
+from company_api import company_api, dependencies
 # instantiate app
 app = FastAPI()
 
@@ -25,3 +25,10 @@ app.include_router(auth.router)
 
 # add rout of apis
 app.include_router(todo_apis.router)
+
+# external rout to company api
+app.include_router(company_api.router,
+                   prefix="/company",
+                   tags=["Company API"],
+                   dependencies=[Depends(dependencies.get_token_header)]
+                   )
